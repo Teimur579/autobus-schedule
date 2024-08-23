e0ce322c79dfcafc39221235ddb6fdf75cc34e2e
Teimur <teimurjakhangirov@gmail.com>
2024-08-23 23:07:21 +0500

    Добавил frontend

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ const port = 3000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const loadBuses = async () => {
-    const data = await readFile(path.join(__dirname, 'buses.json'), "utf-8");
-    return JSON.parse(data);
+    const date = await readFile(path.join(__dirname, 'buses.json'), "utf-8");
+    return JSON.parse(date);
 }
 
 const getNextDeparture = (firstDepartureTime, frequencyMinutes) => {
@@ -25,55 +27,58 @@ const getNextDeparture = (firstDepartureTime, frequencyMinutes) => {
     
     if (now > departure) {
         departure = departure.plus({minutes: frequencyMinutes});
-    }
+    };
 
     const endOfDay = DateTime.now().set({hours: 23, minutes: 59, seconds: 59}).setZone(timeZone);   
 
     if (departure > endOfDay) {
         departure = departure.startOf('day').plus({days: 1}).set({hours: 23, minutes: 59});
-    }
+    };
 
     while (now > departure) {
         departure = departure.plus({minutes: frequencyMinutes});
         
         if (departure > endOfDay) {
             departure = departure.startOf('day').plus({days: 1}).set({hours: 23, minutes: 59});
-        }
-    }
+        };
+    };
 
     return departure;
-}
+};
 
 const sendUpdatedData = async () => {
     const buses = await loadBuses();
-    // const now = DateTime.now().setZone(timeZone);
     const updatedBuses = buses.map((bus) => {
         const nextDeparture = getNextDeparture(bus.firstDepartureTime, bus.frequencyMinutes);
         return {...bus, nextDeparture: {
-            data: nextDeparture.toFormat('yyyy-MM-dd'),
+            date: nextDeparture.toFormat('yyyy-MM-dd'),
             time: nextDeparture.toFormat('HH:mm:ss')
-        }}
-    })
+        }};
+    });
 
     return updatedBuses;
-} 
+}; 
+
+const sortBuses = (buses) => {
+
+    return [...buses].sort((a, b) => new Date(`${a.nextDeparture.date}T${a.nextDeparture.time}Z`) - new Date(`${b.nextDeparture.date}T${b.nextDeparture.time}Z`));
+
+};
 
 app.get('/next-departure', async (req, res) => {
     
     try {
+        
         const updatedBuses = await sendUpdatedData();
-        res.json(updatedBuses);
-        // updatedBuses.forEach(obj => {
-        //     console.log(obj.nextDeparture.data[0])
-        // })
-    }
-
-    catch {
-        res.send('Hello World');
+        const sortedBuses = sortBuses(updatedBuses);
+        res.json(sortedBuses);
+    
+    } catch {
+        res.send('Error');
     }
 
 });
 
 app.listen(port, () => {
-    console.log('Server running on http://localhost:' + port)
+    console.log('Server running on http://localhost:' + port);
 });
